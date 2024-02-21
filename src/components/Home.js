ef563a696b857af1172b982956dc5be5f447b1ae
LaXnZ <sumudithalanz@gmail.com>
2024-02-21 20:25:00 +0530

    about me page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,49 +1,48 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
-    <div className="Mainbackground" id="home">
-      <div className="topBackground"></div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#001517"
-          fillopacity="1"
-          d="M0,192L40,213.3C80,235,160,277,240,293.3C320,309,400,299,480,277.3C560,256,640,224,720,186.7C800,149,880,107,960,122.7C1040,139,1120,213,1200,218.7C1280,224,1360,160,1400,128L1440,96L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"
-        ></path>
-      </svg>
-
-      <div className="container fluids">
-        <div className="row">
-          <div className="col-md-12 col-lg-6">
-            <div className="screenWords">
-              <h1>Hello, I'm Irene.</h1>
-              <p>
-                A full-stack developer. Specializing in building exceptional
-                trending mobile applications and websites. Feel free to take a
-                look at my latest projects. Remotely available UTC−1 to UTC+8.
-                project@irenemmassy.com Tel:+255 065 535 2744
+    <section id="about" style={{ paddingTop: "100px", paddingBottom: "50px" }}>
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-md-6">
+            <div className="about-me">
+              <div className="aboutHorizontalLine"></div>
+              <h1 style={{ fontSize: 24 }}>Hi, I am</h1>
+              <h1 style={{ color: "orange", fontSize: 32 }}>Maverick Edward</h1>
+              <h3 style={{ fontSize: 80, fontWeight: "bold" }}>UI & UX </h3>
+              <h3
+                style={{
+                  fontSize: 80,
+                  fontWeight: "bold",
+                  paddingLeft: "200px",
+                }}
+              >
+                Designer
+              </h3>
+              <p
+                style={{
+                  fontSize: 21,
+                  fontWeight: "initial",
+                  textAlign: "justify",
+                }}
+              >
+                I enjoy creating things that live on the internet, whether that
+                be websites, applications, or anything in between. My goal is to
+                always build products that provide pixel-perfect, performant
+                experiences.
               </p>
-              <div className="twoMainButton">
-                <div className="buttonMain">
-                  <a href="">Mobile Apps</a>
-                  <div className="hrizontalLine"></div>
-                </div>
-                <div className="buttonMain">
-                  <a href="">Websites</a>
-                  <div className="hrizontalLine diff"></div>
-                </div>
-              </div>
             </div>
           </div>
-          <div className="col-md-12 col-lg-6">
-            <div className="screenImage">
-              <img src="./images/main2.png" alt="irene blog" />
+          <div className="col-md-6 pl-5">
+            <div className="aboutMeImg">
+              <img src="/about.png" alt="about me" style={{ width: "80%" }} />
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
-}
+};
 
 export default Home;
