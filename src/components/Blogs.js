4430ab73f76a95a63441d0643a684f3f6223d94c
LaXnZ <sumudithalanz@gmail.com>
2024-02-21 19:12:31 +0530

    about me page

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -21,8 +21,7 @@ const Blogs = () => {
   return (
     <div className="container" id="blogs">
       <div className="article">
-        <h2 className="mb-4">Recent Work</h2>
-        <hr />
+        <h2 className="mb-5"></h2>
         <div className="row">
           {blogs.map((blog) => (
             <div key={blog._id} className="col-lg-4 col-md-6 mb-4">
